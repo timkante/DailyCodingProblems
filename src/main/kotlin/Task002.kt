90fce7a0d6f09037de64d812b5956d6db1eb94ae
Tim Kante <tim@timkante.dev>
2021-02-21 21:14:58 +0100

    :sparkles: added task 2 implementation

--- src/main/kotlin/Task002.kt
+++ src/main/kotlin/Task002.kt
@@ -1,2 +1,9 @@
 package dev.timkante.dcp
 
+val List<Int>.multiplied: List<Int>
+    get() = when (size) {
+        in 0..1 -> this
+        else -> indices.map { elementIndex ->
+            asSequence().filterIndexed { multiplierIndex, _ -> multiplierIndex != elementIndex }.reduce(Int::times)
+        }
+    }
---
+++ src/test/kotlin/Task002KtTest.kt
@@ -0,0 +1,34 @@
+import dev.timkante.dcp.multiplied
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldContainInOrder
+import io.kotest.matchers.shouldBe
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.list
+import io.kotest.property.checkAll
+
+class Task002KtTest : StringSpec({
+    "task example 1 should pass" {
+        listOf(1, 2, 3, 4, 5).multiplied shouldContainInOrder listOf(120, 60, 40, 30, 24)
+    }
+
+    "task example 2 should pass" {
+        listOf(3, 2, 1).multiplied shouldContainInOrder listOf(2, 3, 6)
+    }
+
+    "lists with size less than 2 do return themselves" {
+        checkAll(Arb.list(Arb.int(), 0..1)) { list ->
+            list.multiplied shouldBe list
+        }
+    }
+
+    "list of ints gets converted correctly" {
+        checkAll(Arb.list(Arb.int(), 2..15)) { list ->
+            list.multiplied.withIndex().forEach { (index, convertedListElement) ->
+                convertedListElement shouldBe list
+                    .filterIndexed { elementIndex, _ -> elementIndex != index }
+                    .reduce(Int::times)
+            }
+        }
+    }
+})
